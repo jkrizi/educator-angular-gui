bbb88b2e0c2d7140332b068ef7bef7f259d50240
Jiri Kriz <jirikriz3@gmail.com>
2022-03-16 21:45:45 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { VaultComponent } from './vault/vault.component';
+import { EngineComponent } from './engine/engine.component';
+import { QuestionComponent } from './vault/question/question.component';
+import { QuestionFormComponent } from './vault/question-form/question-form.component';
+import { VaultFilterComponent } from './vault/vault-filter/vault-filter.component';
+import { TemplateSelectorComponent } from './engine/template-selector/template-selector.component';
+import { TestFormComponent } from './engine/test-form/test-form.component';
+import { TagsComponent } from './common/tags/tags.component';
+import { WorkspaceComponent } from './layout/workspace/workspace.component';
+import { QuestionListComponent } from './vault/question-form/question-list/question-list.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    VaultComponent,
+    EngineComponent,
+    QuestionComponent,
+    QuestionFormComponent,
+    VaultFilterComponent,
+    TemplateSelectorComponent,
+    TestFormComponent,
+    TagsComponent,
+    WorkspaceComponent,
+    QuestionListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
---
+++ src/app/vault/vault-filter/vault-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vault-filter',
+  templateUrl: './vault-filter.component.html',
+  styleUrls: ['./vault-filter.component.css']
+})
+export class VaultFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
