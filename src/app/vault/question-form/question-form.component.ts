620897948a29fb2befd4f57fc913f4d83cd20829
Jiri Kriz <jirikriz3@gmail.com>
2022-04-01 11:17:50 +0200

    Add easy question switching with opened editor

--- src/app/vault/question-form/question-form.component.ts
+++ src/app/vault/question-form/question-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
 import {Question} from "../../models/question.model";
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Answer} from "../../models/answer.model";
@@ -8,7 +8,7 @@ import {Answer} from "../../models/answer.model";
   templateUrl: './question-form.component.html',
   styleUrls: ['./question-form.component.css']
 })
-export class QuestionFormComponent implements OnInit {
+export class QuestionFormComponent implements OnChanges {
 
   taughtSubjects: string[] = ['', 'Matematika', 'Fyzika', 'Chemie'];
   knownTags: string[] = ['Test1', 'Zaklady', 'Vzorce', 'Neorganicka', '1. pololeti', '3. rocnik'];
@@ -23,26 +23,16 @@ export class QuestionFormComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.usedTags = Object.assign([], this.question.tags);
-
-    this.form = new FormGroup({
-      subject: new FormControl(this.question.subject, [Validators.required]),
-      tags: new FormControl(this.usedTags),
-      difficulty: new FormControl(this.question.difficulty),
-      title: new FormControl(this.question.title),
-      assignment: new FormControl(this.question.assignment),
-      answers: new FormArray(this.question.answers.map(
-        (answer: Answer) => QuestionFormComponent.createAnswerGroup(answer)
-      ))
-    });
+  ngOnChanges(changes: SimpleChanges): void {
+    this.question = changes.question.currentValue;
+    this.fillForm();
   }
 
   getControls(form: FormGroup, arrayName: string): FormGroup[] {
     return (form.get(arrayName) as FormArray).controls.map(abstractControl => <FormGroup>abstractControl);
   }
 
-  public closeForm() {
+  closeForm() {
     this.form.reset();
     this.closeEditor.emit();
   }
@@ -58,6 +48,21 @@ export class QuestionFormComponent implements OnInit {
 
   addAnswer(newAnswer: Answer) {
     this.formAnswers.push(QuestionFormComponent.createAnswerGroup(newAnswer))
+  }
+
+  private fillForm() {
+    this.usedTags = Object.assign([], this.question.tags);
+
+    this.form = new FormGroup({
+      subject: new FormControl(this.question.subject, [Validators.required]),
+      tags: new FormControl(this.usedTags),
+      difficulty: new FormControl(this.question.difficulty),
+      title: new FormControl(this.question.title),
+      assignment: new FormControl(this.question.assignment),
+      answers: new FormArray(this.question.answers.map(
+        (answer: Answer) => QuestionFormComponent.createAnswerGroup(answer)
+      ))
+    });
   }
 
   private get formAnswers(): FormArray {
